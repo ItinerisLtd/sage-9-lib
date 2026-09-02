f125779f819a50ffecad94f3346e1e44db06e0e6
Lee Hanbury <lee.hanbury@gmail.com>
2026-09-02 16:09:00 +0000

    feat(deps): bump illuminate/view and illuminate/config to ^12.0 (#1)

--- Container.php
+++ Container.php
@@ -1,9 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Roots\Sage;
 
 use Illuminate\Container\Container as BaseContainer;
 
+use function register_shutdown_function;
+
 class Container extends BaseContainer
 {
+    protected array $terminatingCallbacks = [];
+
+    public function terminating(callable $callback): static
+    {
+        if (empty($this->terminatingCallbacks)) {
+            register_shutdown_function([$this, 'terminate']);
+        }
+
+        $this->terminatingCallbacks[] = $callback;
+
+        return $this;
+    }
+
+    public function terminate(): void
+    {
+        foreach ($this->terminatingCallbacks as $callback) {
+            $callback();
+        }
+
+        $this->terminatingCallbacks = [];
+    }
 }
